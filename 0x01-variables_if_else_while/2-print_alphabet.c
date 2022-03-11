b9b7c87c8dabc4e676e5939912e7ad237f170d2d
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-11 13:41:34 -0800

    C is Awesome and Cool

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+  
+    // Write the Character to stdout
+    for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+return (0);
+}
+
