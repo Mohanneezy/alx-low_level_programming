ec6026c44aa35eab6767a69d04c7a9f0427c3ada
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-11 13:56:05 -0800

    C is Awesome and Cool

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,12 @@
 int main(void)
 {
 char ch = 'a';
+char upper = 'A';
 
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
+for (upper = 'A'; upper <= 'Z'; upper++)
+putchar(upper);
 putchar('\n');
 
 return (0);
