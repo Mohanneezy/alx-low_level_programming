24c5b7795036568664358fd8a3d216014334ac7d
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 11:26:23 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for lowercase
+ * @n: n is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int print_last_digit(int n)
+{
+int num;
+num=n%10;
+return (num);	
+}
