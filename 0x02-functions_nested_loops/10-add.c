c64754300bf549832760d4f417a0fa39cf4b95ce
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 11:39:17 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @num1: num 1 is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int add(int num1, intnum2)
+{
+int result = num1 + num2;
+return (result);	
+}
