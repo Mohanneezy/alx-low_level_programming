d443c34a3c95794e8841b78f1120cd16bea45176
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 11:14:11 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - checks for absolute
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int _abs(int)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{	
+return (-n);
+}
+else
+{	
+return (n);
+}
+return (0);	
+}
