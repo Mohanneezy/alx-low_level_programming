18f29a34dbbd13b6f25d2bb6b09b05876b217869
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 09:46:31 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('a');
+	_putchar('l');
+	_putchar('x');
+	_putchar('i');
+	_putchar('s');
+	_putchar('c');
+	_putchar('o');
+	_putchar('o');
+	_putchar('l');
+	_putchar('\n');
+
+	return (0);
+}
