6ce8f717c5da9b4c7901d9acae4347203c506b63
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 09:27:30 -0700

    we are getting hang of C and loving it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet()
 {
     char alpha ='a';
     for (alpha= 'a' ; alpha<='z'; alpha++)
