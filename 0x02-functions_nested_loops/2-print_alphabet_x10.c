7f5b73e4ed5ddeb53dc84074c148242b68b15ab4
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 10:09:18 -0700

    we are getting hang of C and loving it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,8 @@
 void print_alphabet_x10(void)
 {
     char alpha ='a';
-    for (int i=0 ; i<10 ; i++)
+    int i;
+    for (i=0 ; i<10 ; i++)
     for (alpha= 'a' ; alpha<='z'; alpha++)
     _putchar(alpha);
     _putchar('\n');
