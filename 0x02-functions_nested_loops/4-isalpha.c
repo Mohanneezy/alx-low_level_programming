688260d776a99c0bc48ba39ef4482038a4c1d96b
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 10:46:11 -0700

    we are getting hang of C and loving it

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 #include "main.h"
+
 /**
  * _islower - checks for letter
  * @c: c is an ascii character
  *
  * Return: 1 if letter and Return:0 if not 
  */
-
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
