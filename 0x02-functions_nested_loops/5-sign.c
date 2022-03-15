36d9fe7791978fd50e0cb109774bc2595f094843
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 10:55:34 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _int print_sign - sign for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n == 0 )
+{
+_putchar('0');
+return (0);
+}
+}
