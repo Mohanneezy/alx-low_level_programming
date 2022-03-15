1495147f0dfd6067eae89a3870ec8cb4e8858af4
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 11:55:53 -0700

    we are getting hang of C and loving it

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 for lowercase
+ * @n: n is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i=n; i < 98; i++)
+_putchar(",");
+_putchar(" ");
+}
+else
+{
+for (i=98; i < n; i++)
+_putchar(",");
+_putchar(" ");
+}
+}
