524d0c7bd53392b6e504957da087e91f38b7a30d
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-15 12:37:55 -0700

    we are getting hang of C and loving it

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,23 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
+
 /**
- * print_to_98 for lowercase
- * @n: n is an ascii character
- *
- * Return: 1 if lowercase
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
-int i;
-if (n == 98)
+if (n >= 98)
 {
+while (n > 98)
+printf("%d, ", n--);
 printf("%d\n", n);
-}
-else if (n > 98)
-{
-for (i = n; i >= 98; i--)
-printf("%d, ", i);
 }
 else
 {
-for (i = 98; i >= n; i--)
-printf("%d, ", i);
-}}
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
