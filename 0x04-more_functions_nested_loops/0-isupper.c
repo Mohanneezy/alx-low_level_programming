17002907e18ff27498b3d6f805a205303920eb36
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-17 10:30:08 -0700

    we are getting hang of alx and loving it

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - Checks for uppercase characters.
  * @c: The character to be checked.
@@ -7,7 +8,7 @@
  */
 int _isupper(int c)
 {
-    if (c <= 'A' && c <= 'Z')
+    if (c >= 'A' && c <= 'Z')
         return (1);
 
   
