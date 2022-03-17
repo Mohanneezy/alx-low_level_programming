dea562c53c6e08a3fee3beedd6a10762352e3570
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-17 11:06:41 -0700

    we are getting hang of alx and loving it

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * mult - Checks for a digit (0-9).
- * @a: The number to be checked.
- * @b: The number to be checked.
+ * mul - Checks for a digit (0-9).
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
  *
  * Return: 1 if the number is a digit, 0 otherwise.
  */
 int mul(int a, int b)
 {
-   
-	return (a * b);
+        return (a * b);
 }
