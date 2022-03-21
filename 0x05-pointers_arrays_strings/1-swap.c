150c08a8931b67238d168f11d06e30f0393ab224
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-21 09:12:10 -0700

    we are getting hang of alx and loving it

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  swap_int - Checks for uppercase characters.
+ * @a: The character to be checked.
+ * @b: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void swap_int(int *a, int *b)
+{
+int x;
+t = *a;
+*a = *b;
+*b = t;
+
+}
+
