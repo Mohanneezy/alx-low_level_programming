68132881d8beb1341aec5215fc1766cde810f5e9
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-21 10:30:32 -0700

    we are getting hang of alx and loving it

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+int len = 0, i;
+while (*str++)
+len++;
+for (i = len-1; i >= 0; i--)
+{
+printf("%c" ,str[i]);
+}
+putchar('\n');
+return 0;
+}
