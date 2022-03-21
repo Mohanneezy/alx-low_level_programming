f31b3eac356b01808236ac03bc5ab053d7921fe5
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-21 09:58:53 -0700

    we are getting hang of alx and loving it

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - Checks for uppercase characters.
+ * @str: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void _puts(char *str)
+{
+printf ("%s\n" ,str);
+}
