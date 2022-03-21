515099a3f88a669c9ac31815a85b961cb35da69c
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-21 09:26:54 -0700

    we are getting hang of alx and loving it

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Checks for uppercase characters.
+ * @s: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _strlen(char *s)
+{
+int len;
+int i = 0;
+for (s = len; *s != '\0'; s++) {
+    i++;
+  }
+  printf("%d", i);
+}
+
