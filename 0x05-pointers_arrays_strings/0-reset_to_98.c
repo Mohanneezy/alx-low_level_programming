080f1c3495524e12b4bd9894d5708886932b0b3d
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-21 08:42:18 -0700

    we are getting hang of alx and loving it

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  reset_to_98 - Checks for uppercase characters.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void reset_to_98(int *n)
+{
+ *n = 98;
+  int n;
+ printf("n=%d\n", n);
+ n = *n;
+printf("n=%d\n", n);
+}
