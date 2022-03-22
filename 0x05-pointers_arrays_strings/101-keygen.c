26b2023f54d97d9daf0d4dda0e3b5d97017002e7
Nahom <99352872+Mohanneezy@users.noreply.github.com>
2022-03-22 13:11:52 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,12 +10,14 @@
 
 int main(void)
 {
+	int index = 0;
 	srand((unsigned int)(time(NULL)));
 
-	int index = 0;
+	
 	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
 	for(index = 0; index < 12; index++)
 	{
 		printf("%c", characters[rand() % (sizeof characters - 1)]);
 	}
+	return 0;
 }
