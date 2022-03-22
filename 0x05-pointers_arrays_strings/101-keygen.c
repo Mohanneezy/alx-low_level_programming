bafb680faa4ddf5b40b9571d529d59cee2ae6e74
Nahom <99352872+Mohanneezy@users.noreply.github.com>
2022-03-22 13:00:45 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,67 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <math.h>
-void randomPasswordGeneration(int N)
-{
-    
-    int i = 0;
-    char num[] = "0123456789";
-  char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-   
-    char symbols[] = "!@#$^&*?";
-  
-    
-    char password[N];
-  
-    int randomizer = 0;
-
-    srand((unsigned int)(time(NULL)));
-  
-  
-
-    
-    
-    randomizer = rand() % 4;
-  
-    
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = num[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
-  
+#include <time.h>
 
 int main()
 {
-
-    int N = 10;
-  
-    
-    randomPasswordGeneration(N);
-  
-    return 0;
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[12];
+    for(i = 0; i < 12; i++) {
+    pass[i] = 33 + rand() % 94;
+}
+pass[i] = '\0';
+printf("%s\n",pass);
+    printf("\n\n");
 }
