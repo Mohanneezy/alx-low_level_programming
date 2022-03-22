13f887f1e3140555b13408546c57ef9afe8e2123
Nahom <99352872+Mohanneezy@users.noreply.github.com>
2022-03-22 12:47:33 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,7 @@ void randomPasswordGeneration(int N)
     srand((unsigned int)(time(NULL)));
   
  
-    char numbers[] = "0123456789";
+    char num[] = "0123456789";
   
 
     char letter[] = "abcdefghijklmnoqprstuvwyzx";
@@ -33,7 +33,7 @@ void randomPasswordGeneration(int N)
     for (i = 0; i < N; i++) {
   
         if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
+            password[i] = num[rand() % 10];
             randomizer = rand() % 4;
             printf("%c", password[i]);
         }
