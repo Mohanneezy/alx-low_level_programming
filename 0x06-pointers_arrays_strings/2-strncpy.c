05cad7faf39454e9b7a211fb3a5445f2c7d10d4e
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-24 05:12:33 -0700

    im so done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two strings
+ * @dest: destination char
+ * @src: source char
+ * @n: byte size
+ * Return: new char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int length, j;
+length = 0;
+while (dest[length] != '\0')
+{
+++length;
+}
+for (j = 0; j < n && src[j] != '\0'; ++j, ++length)
+{
+dest[length] = src[j];
+}
+dest[length] = '\0';
+return (dest);
+}
