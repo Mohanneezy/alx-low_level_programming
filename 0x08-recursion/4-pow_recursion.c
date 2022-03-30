8660ecbee43f642897fbd4ba9daff2d5a1e5a951
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-30 10:57:47 -0700

    we are commiting the second task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _pow_recursion* @s: The string to be printed.
+ * @y: The string to be powered.
+ * @y: The string to poweree.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return(-1);
+return(x * _pow_recursion( x, y -1));
+}
