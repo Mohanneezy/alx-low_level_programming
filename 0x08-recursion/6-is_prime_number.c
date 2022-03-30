feb2fff701b7876eafdcd4ef523ab02928ea690e
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-30 12:17:05 -0700

    we are commiting the second task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_prime_number - Finds if n is prime number.
+ * @n: The number to find the square root of.
+ *
+ * Return: If the number is prime number.
+ */
+
+int is_prime_number(int n)
+{
+if (n % is_prime_number( n-1 ))
+return (1);
+else
+return (0);
+}
