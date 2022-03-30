2d9d54af38990ac0e703f0f5888d364dc728db6f
Mohanneezy <lilnahublacksmith@gmail.com>
2022-03-30 09:57:14 -0700

    we are commiting the second task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,21 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-char revstr[500000];
-char *rvptr = revstr;
-int i = -1;
-while (*s)
+if (s != '\n')
 {
-s++;
-i++;
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
-while (i >= 0)
-{
-s--;
-*rvptr = *s;
-rvptr++;
---i;
-}
-*rvptr = '\0';
-printf("%s\n", revstr);
 }
