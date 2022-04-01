1de6c7234f318445f08e8a66a35f5e939d8180ec
Mohanneezy <lilnahublacksmith@gmail.com>
2022-04-01 13:28:34 -0700

    this is arg and argv[]

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int argc, char *argv[])
+{
+int i, x, y, product;
+if (argc != 3)
+printf("Error \n");
+for (i = 1; i < argc; i++)
+{
+x = atoi(argv[1]);
+y = atoi(argv[1 + 1]);
+product = x *y;
+printf("%d\n", product);
+}
+return (0);
+}
