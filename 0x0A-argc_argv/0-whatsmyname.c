cb4d004d8047402aee245f168f04b72cb13f0fb2
Mohanneezy <lilnahublacksmith@gmail.com>
2022-04-01 12:38:54 -0700

    this is arg and argv[]

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-printf("%d\n" ,arg[0]);
+int i;
+for (i = 0;  i < argc; i++)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
 }
